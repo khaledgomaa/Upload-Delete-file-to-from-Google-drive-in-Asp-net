391998c6a0c506d01361cfb4c7f53e885871d67d
KhaledGomaa <KhaledGomaa670@gmail.com>
2020-05-07 16:26:29 +0200

    googledrive class and calling this class inside my web api

---
+++ ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CSLY.Repository;
+using CSLY.Models;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.Threading;
+using CSLY.ViewModel;
+using CSLY.googledriveapi;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using System.Web;
+
+namespace CSLY.Controllers.api
+{
+    public class ProductsController : ApiController 
+    {
+        [HttpPost]
+        public IHttpActionResult                     AddProduct(string filePath)
+        {
+            string fileId;
+            fileId = googledrive.                     FileUpload(filePath);
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult                             DeleteProduct(string fileId)
+        {
+            if(file != null)
+                googledrive.                             DeleteFile(file);
+            return Ok();
+        }
+
+        
+    }
+}
---
+++ googledrive.cs
@@ -0,0 +1,93 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CSLY.googledriveapi
+{
+    
+    public class googledrive
+    {
+        public static string[] Scopes = { DriveService.Scope.Drive };
+
+        public static DriveService GetService()
+        {
+            UserCredential credential;
+            var path = HttpContext.Current.Server.MapPath("/credentials.json");
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                // Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            //Create Drive API service.
+            DriveService service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GoogleDriveRestAPI-v3",
+            });
+
+            return service;
+        }
+
+        public static string FileUpload(string file)
+        {
+                DriveService service = GetService();
+                var FileMetaData = new Google.Apis.Drive.v3.Data.File();
+                FileMetaData.Name = Path.GetFileName(file);
+                FileMetaData.MimeType = "image/";
+
+                FilesResource.CreateMediaUpload request;
+
+                using (var stream = new System.IO.FileStream(file, System.IO.FileMode.Open))
+                {
+                    request = service.Files.Create(FileMetaData, stream, FileMetaData.MimeType);
+                    request.Fields = "id";
+                    request.Upload();
+                }
+                var file_ = request.ResponseBody;
+
+                return file_.Id;
+        }
+        
+        public static void DeleteFile(string files)
+        {
+            DriveService service = GetService();
+            try
+            {
+                // Initial validation.
+                if (service == null)
+                    throw new ArgumentNullException("service");
+
+                if (files == null)
+                    throw new ArgumentNullException(files);
+
+                // Make the request.
+                service.Files.Delete(files).Execute();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Request Files.Delete failed.", ex);
+            }
+        }
+
+    }
+}
